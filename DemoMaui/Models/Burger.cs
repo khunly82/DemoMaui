0f41573d00a06ab2cdb9b805657bbc822617a1b4
Khun <lykhun@gmail.com>
2025-12-17 11:27:16 +0100

    Burger

--- DemoMaui/Models/Burger.cs
+++ DemoMaui/Models/Burger.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DemoMaui.Models
+﻿namespace DemoMaui.Models
 {
     public class Burger
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public decimal Price { get; set; }
-        public bool IsVeagan { get; set; }
+        public bool IsVegan { get; set; }
         public string ImageUrl { get; set; } = null!;
     }
 }
